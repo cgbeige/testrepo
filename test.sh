cadf242e2f393bf160813c6258083e8480e831dd
cgbeige <daveg@3eige.com>
2016-05-28 10:37:05 -0700

    updated new
    removed some stuff

--- test.sh
+++ test.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-SOURCEDIR=`dirname "${BASH_SOURCE[0]}"`
-HOUDINIVERSION="15.5.488" 
-sudo echo "Installing Houdini ${HOUDINIVERSION}"
-hdiutil mount "$SOURCEDIR/houdini-${HOUDINIVERSION}-macosx_x86_64_clang6.1_10.10.dmg"
-sudo installer -pkg /Volumes/Houdini/Houdini.pkg -target /
-hdiutil unmount /Volumes/Houdini/
-
-##LICENSING HOUDINI: http://www.sidefx.com/faq/license-management/#how-do-i-use-sesictrl
-cd /Library/Frameworks/Houdini.framework/Versions/${HOUDINIVERSION}/Resources/houdini/sbin
-
 ##INSTALL IMAGEMAGICK
 sudo installer -pkg "$SOURCEDIR/ImageMagick-6.9.1-0.pkg" -target /
 
